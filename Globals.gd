0131019b2735021e2ca4d3c59736f4e91a5eeef3
svntax <endless.answers@gmail.com>
2020-10-02 22:42:36 -0400

    Basic project setup

---
+++ Globals.gd
@@ -0,0 +1,8 @@
+extends Node
+
+func _ready():
+	OS.window_size = Vector2(640, 480)
+	# Center the window after resizing
+	var screen_size = OS.get_screen_size()
+	var window_size = OS.get_window_size()
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
