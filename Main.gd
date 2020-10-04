f55b68e12c1638c4d11978f15d721e3f0ce03cd7
svntax <endless.answers@gmail.com>
2020-10-04 19:05:01 -0400

    WIP title screen.

--- Main.gd
+++ Main.gd
@@ -1,4 +1,25 @@
 extends Node2D
 
+onready var animation_player = $AnimationPlayer
+onready var intro_timer = $IntroTimer
+
+onready var cutscene_finished = false
+
 func _ready():
-	pass
+	animation_player.play("intro")
+	intro_timer.start()
+
+func _process(delta):
+	if Input.is_action_just_pressed("left_click"):
+		# Skip intro cutscene
+		intro_timer.stop()
+		cutscene_finished = true
+		animation_player.stop()
+		animation_player.play("normal")
+
+func _on_IntroTimer_timeout():
+	animation_player.play("intro_transition")
+	animation_player.queue("normal")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	cutscene_finished = true
