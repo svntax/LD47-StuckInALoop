46a7009c8e584d5a9d86f69bdcce1eb0834c1139
svntax <endless.answers@gmail.com>
2020-10-05 03:33:28 -0400

    Added particles when a pin breaks. Fixed bug with balloon not being removed.

--- SoundHandler.gd
+++ SoundHandler.gd
@@ -2,3 +2,4 @@ extends Node
 
 onready var select_sfx = $SelectSound
 onready var hit_sfx = $HitSound
+onready var pin_break_sfx = $PinBreakSound
--- Wheel/Pin.gd
+++ Wheel/Pin.gd
@@ -1,5 +1,10 @@
 extends StaticBody2D
 
+onready var area_detect = $AreaDetect
+onready var sprite = $Sprite
+onready var particles = $Particles
+onready var death_timer = $DeathTimer
+
 export (bool) var permanent = false
 onready var broken = false
 
@@ -7,6 +12,7 @@ func _physics_process(_delta):
 	if Input.is_action_just_pressed("left_click"):
 		var mouse_pos = get_global_mouse_position()
 		if !broken and mouse_pos.distance_to(global_position) <= 8:
+			SoundHandler.pin_break_sfx.play()
 			break_pin()
 
 func break_pin():
@@ -17,7 +23,13 @@ func break_pin():
 		return
 	broken = true
 	
-	queue_free()
+	sprite.hide()
+	collision_mask = 0
+	collision_layer = 0
+	area_detect.collision_layer = 0
+	area_detect.collision_mask = 0
+	particles.emitting = true
+	death_timer.start()
 
 func _on_AreaDetect_body_entered(body):
 	if body.is_in_group("Players"):
@@ -27,3 +39,7 @@ func _on_AreaDetect_body_entered(body):
 			break_pin()
 		else:
 			body.get_parent().play_hit_sound()
+
+
+func _on_DeathTimer_timeout():
+	queue_free()
